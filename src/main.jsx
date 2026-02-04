74b5e192b1e85c1d63ea932f0f97ed55d2773d9e
100495730 <158482211+100495730@users.noreply.github.com>
2026-02-04 18:38:55 +0100

    Replace App component with Calculator integration
    Refactored main component to include Calculator.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import Calculator from './components/Calculator'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  )
+}
